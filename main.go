00aed51966b56f8d016bd013c66204ee667e0d8c
olandebn <olan.debruyne@ynov.com>
2025-09-23 10:08:51 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func main() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, serveur Go fonctionne ! 🚀")
-	})
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Erreur serveur:", err)
-	}
-}
+var game *Game
