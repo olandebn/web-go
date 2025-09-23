c694404414acb43c8c3d5a259d7b9e78f9fc63de
olandebn <olan.debruyne@ynov.com>
2025-09-23 10:08:11 +0200

    Update game.go

--- game.go
+++ game.go
@@ -1,6 +1,22 @@
 package main
 
 const (
-	Rows   = 6
-	Colums = 7
+	Rows    = 6
+	Columns = 7
 )
+
+type Player int
+
+const (
+	Empty Player = iota
+	Player1
+	Player2
+)
+
+type Game struct {
+	Grid      [Rows][Columns]Player
+	Current   Player
+	GameOver  bool
+	Winner    Player
+	MoveCount int
+}
