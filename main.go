6d285c0dfb1053bda67002c8bb80daf0360dfdcb
olandebn <olan.debruyne@ynov.com>
2025-09-23 10:09:51 +0200

    g

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-var game *Game
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Bienvenue sur Power 4 Web 🎮 !")
+	})
+
+	fmt.Println("Serveur lancé sur http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Erreur serveur:", err)
+	}
+}
